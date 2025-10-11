72fd01bed114c206291dfdaae0f8ccaae3042224
rosics-code <roblox888i12345@gmail.com>
2025-10-11 18:09:22 -0400

    Create version.js

---
+++ version.js
@@ -0,0 +1,10 @@
+  <script>
+  const SITE_VERSION = "1.0.0";
+
+  document.querySelectorAll('link[rel="stylesheet"], script[src]').forEach(el => {
+    const url = new URL(el.href || el.src, location.origin);
+    url.searchParams.set('v', SITE_VERSION);
+    if (el.tagName === 'LINK') el.href = url;
+    else el.src = url;
+  });
+</script>
