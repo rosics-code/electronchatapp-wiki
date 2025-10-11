f4c8c077bb7a524278f73d4967099cad856fbcab
rosics-code <roblox888i12345@gmail.com>
2025-10-11 18:22:07 -0400

    Update version.js

--- version.js
+++ version.js
@@ -1,10 +1,11 @@
-  <script>
-  const SITE_VERSION = "1.0.5";
+const SITE_VERSION = "1.0.0";
 
-  document.querySelectorAll('link[rel="stylesheet"], script[src]').forEach(el => {
-    const url = new URL(el.href || el.src, location.origin);
-    url.searchParams.set('v', SITE_VERSION);
-    if (el.tagName === 'LINK') el.href = url;
-    else el.src = url;
-  });
-</script>
+document.querySelectorAll('link[rel="stylesheet"], script[src]').forEach(el => {
+  if (el.src && el.src.includes('version.js')) return;
+
+  const url = new URL(el.href || el.src, location.origin);
+  url.searchParams.set('v', SITE_VERSION);
+
+  if (el.tagName === 'LINK') el.href = url;
+  else if (el.tagName === 'SCRIPT') el.src = url;
+});
